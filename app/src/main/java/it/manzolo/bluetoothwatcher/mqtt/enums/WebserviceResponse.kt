d43d22360975d0115f0457e227fee308c0f236d7
Andrea Manzi <manzolo@libero.it>
2024-07-06 14:46:01 +0200

    Remove unused code

--- app/src/main/java/it/manzolo/bluetoothwatcher/mqtt/enums/WebserviceResponse.kt
+++
@@ -1,9 +0,0 @@
-package it.manzolo.bluetoothwatcher.mqtt.enums
-
-interface WebserviceResponse {
-    companion object {
-        const val ERROR = "ERROR"
-        const val OK = "OK"
-        const val TOKEN_EXPIRED = "TOKEN_EXPIRED"
-    }
-}
